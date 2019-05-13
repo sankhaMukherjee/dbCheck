d1eb38f7bddf413e3f7a50624c7975da3f46fa2e
Sankha Subhra Mukherjee <sankha.mukherjee@gmail.com>
2019-05-13 15:44:17 +0800

    initializing the cohort geenration program

---
+++ src/modules/cohort/cohort.py
@@ -0,0 +1,62 @@
+from logs import logDecorator as lD 
+import json, pprint
+
+from psycopg2.sql import SQL, Identifier, Literal
+from lib.databaseIO import pgIO
+
+config = json.load(open('../config/config.json'))
+logBase = config['logging']['logBase'] + '.modules.cohort.cohort'
+
+
+@lD.log(logBase + '.generateSchema')
+def generateSchema(logger, schemaName):
+    '''Generates the required schema
+    
+    This function generates the required schema
+    
+    Parameters
+    ----------
+    logger : {logging.Logger}
+        The logger used for logging error information
+    '''
+
+    try:
+        query = SQL('CREATE SCHEMA if not exists {}').format( Identifier(schemaName) )
+        pgIO.commitData( query, schemaName )
+    except Exception as e:
+        logger.error(f'Unable to generate the schema [{schemaName}]: {e}')
+
+    return
+
+@lD.log(logBase + '.main')
+def main(logger, resultsDict):
+    '''main function for cohort
+    
+    This function finishes all the tasks for the
+    main function. This is a way in which a 
+    particular module is going to be executed. 
+    
+    Parameters
+    ----------
+    logger : {logging.Logger}
+        The logger used for logging error information
+    resultsDict: {dict}
+        A dintionary containing information about the 
+        command line arguments. These can be used for
+        overwriting command line arguments as needed.
+    '''
+
+    print('='*30)
+    print('Main function of cohort')
+    print('='*30)
+    print('We get a copy of the result dictionary over here ...')
+    pprint.pprint(resultsDict)
+
+    cfg = json.load(open('../config/modules/cohort.json'))['params']
+    generateSchema(cfg['schema'])
+
+    print('Getting out of cohort')
+    print('-'*30)
+
+    return
+
